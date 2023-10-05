09a6c6c5b31d0b4082f34437d014da4f512a6416
DanielPena229185 <daniel.pena229185@potros.itson.edu.mx>
2023-10-04 18:25:03 -0700

    Proyecto dentro del repositorio

---
+++ src/main/java/org/sistem/BusComponent.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.sistem;
+
+/**
+ *
+ * @author Daniel Armando Peña Garcia ID:229185
+ */
+public class BusComponent {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
